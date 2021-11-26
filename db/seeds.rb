2a4e2568adc36a6f3f5db1e3cf9307d854c973c1
parbjotb <parbjotb@gmail.com>
2021-11-26 08:47:11 -0600

    Populated db with products and manufacturers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+Product.delete_all
+Manufacturer.delete_all
+
+filename = Rails.root.join("db/kitchen_products.csv")
+
+puts "Loading products from csv file: #{filename}"
+
+csv_data = File.read(filename)
+
+products = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+products.each do |p|
+  # puts p["name"]
+  # as we create products, we need to have the manufacturer its tied to
+  manufacturer = Manufacturer.find_or_create_by(name: p["manufacturer"])
+
+  if manufacturer && manufacturer.valid?
+    # create products here
+    product = manufacturer.products.create(
+      name:        p["name"],
+      price:       p["price"],
+      description: p["description"]
+    )
+
+    # this is like saying product && product.valid?
+    puts "Invalid product #{p['name']}" unless product&.valid?
+  else
+    puts "Invalid Manufacturer, #{p['manufacturer']} for product: #{p['name']}"
+  end
+end
+
+puts "Created #{Manufacturer.count} manufacturers"
+puts "Created #{Product.count} products"
