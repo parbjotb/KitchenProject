fb1d07ac67d420161f9b6d869b4a9eb591a16ac0
parbjotb <parbjotb@gmail.com>
2021-11-27 07:09:44 -0600

    made page links dynamic in the footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  helper_method :cms_pages
+  # by making it a helper method, we can call from anywhere
+
+  def cms_pages
+    Page.all
+  end
 end
