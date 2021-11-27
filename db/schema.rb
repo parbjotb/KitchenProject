f296ac9800bdf56447bf35e777a18cf3f64e0f24
parbjotb <parbjotb@gmail.com>
2021-11-27 07:20:21 -0600

    added category model and joiner table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_222458) do
+ActiveRecord::Schema.define(version: 2021_11_27_131951) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "manufacturers", force: :cascade do |t|
     t.string "name"
@@ -26,6 +32,15 @@ ActiveRecord::Schema.define(version: 2021_11_26_222458) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "product_categories", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.bigint "price"
@@ -36,5 +51,7 @@ ActiveRecord::Schema.define(version: 2021_11_26_222458) do
     t.index ["manufacturer_id"], name: "index_products_on_manufacturer_id"
   end
 
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
   add_foreign_key "products", "manufacturers"
 end
