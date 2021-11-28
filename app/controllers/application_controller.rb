830aa8faef939aa7fd6ad00ee1584216cd3bef18
parbjotb <parbjotb@gmail.com>
2021-11-28 07:33:43 -0600

    modified application controller for shopping cart sessiosn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
-  helper_method :cms_pages
+  before_action :initialize_session
+  helper_method :cms_pages, :cart
   # by making it a helper method, we can call from anywhere
+
+  private
+
+  def initialize_session
+    # this initializes shopping cart if it doesn't exist already
+    session[:shopping_cart] ||= [] # empty array of product id's
+  end
+
+  def cart
+    # pass an array of product id's, get a collection of products in cart
+    Product.find(session[:shopping_cart])
+  end
 
   def cms_pages
     Page.all
