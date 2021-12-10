dc028fd7ccf394f126323143a843ed8281bc58f5
parbjotb <parbjotb@gmail.com>
2021-12-10 15:02:28 -0600

    added references to provinces, order, orderitems, orderstatus models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+  belongs_to :order_status
+  belongs_to :users
+  has_many :order_items
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   belongs_to :manufacturer
   has_many :product_categories
   has_many :categories, through: :product_categories
+  has_many :order_items
 
   validates :name, :price, :description, presence: true
   validates :price, numericality: true
